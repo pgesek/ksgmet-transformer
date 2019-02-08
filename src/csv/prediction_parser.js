b2759e08d304523c379cfaed29a6e9c88fb0ceb3
Paweł Gesek <pgesek@gmail.com>
2019-02-08 01:26:49 +0100

    Prediction parser logi

--- src/csv/prediction_parser.js
+++ src/csv/prediction_parser.js
@@ -10,7 +10,34 @@ class PredictionParser {
         this.files = csvFileBuilder.build();
     }
 
-    
+    _readPredictionUnits() {
+        let unit;
+        do {
+            unit = this._readSinglePredictionUnit();
+            
+            if (unit) {
+
+            }
+        } while (unit);
+    }
+
+    _readSinglePredictionUnit() {
+        const unit = {};
+        let anyFileHadValue = false;
+
+        files.forEach(file => {
+            const val = file.nextCell();
+            let valAsNum = null;
+            if (val) {
+                valAsNum = new Number(val);
+                anyFileHadValue = true;
+            }
+
+            unit[file.varName] = valAsNum;
+        });
+
+        return anyFileHadValue ? unit : null;
+    }
 }
 
 module.exports = PredictionParser;
