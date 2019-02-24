0d270f1847518b489b7f6ed2b27ea1a74de9cd92
Paweł Gesek <pgesek@gmail.com>
2019-02-24 22:48:45 +0100

    Fix missing comma

--- src/util/settings.js
+++ src/util/settings.js
@@ -21,7 +21,7 @@ const settings = Object.freeze({
 
     TIMEZONE: getSetting('TIMEZONE', 'Europe/Warsaw'),
     PRED_MAX_MINUTES_DIFF: parseInt(getSetting('PRED_MAX_MINUTES_DIFF', '300')),
-    ACTUAL_MAX_MINUTES_DIFF: parseInt(getSetting('ACTUAL_MAX_MINUTES_DIFF', '240'))
+    ACTUAL_MAX_MINUTES_DIFF: parseInt(getSetting('ACTUAL_MAX_MINUTES_DIFF', '240')),
 
     TARGET_BUCKET: getSettings('S3_TARGET_BUCKET_NAME', 'ksgmet-processed'),
     TARGET_PREFIX: getSettings('S3_TARGET_PREFIX', 'csv'),
