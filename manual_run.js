2c3434389a29a280da39f42f07c467bfaf9547ac
Paweł Gesek <pgesek@gmail.com>
2019-02-24 23:05:45 +0100

    Clean up manual run script

--- manual_run.js
+++ manual_run.js
@@ -1,9 +1,5 @@
 const Transformer = require('./src/transformer.js');
 const log = require('./src/util/log.js');
-const S3finder = require('./src/s3/s3_finder.js');
-const moment = require('moment-timezone');
 
-const finder = new s3finder();
-
-//const transformer = new Transformer();
-//transformer.transform('2018_10_24_20_00_50').then(() => log.info('Script finished'));
+const transformer = new Transformer();
+transformer.transform('2018_10_24_20_00_50').then(() => log.info('Script finished'));
